9c14bb4eb2a4ae16e12eca0eb2bac1fcdae3a40f
Sean Melody <seandmelody@gmail.com>
2020-12-09 16:24:12 -0800

    listening at localhost 5003

--- Express.js
+++ Express.js
@@ -1,1 +1,14 @@
-console.log("hi!")
+// console.log("hi!")
+const express = require("express");
+const app = express();
+const PORT = 5003;
+// const apiRoutes = require("./api-routes");
+// const clientRoutes = require("./routes/client-routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use("/api", apiRoutes);
+// app.use("/", clientRoutes);
+
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
