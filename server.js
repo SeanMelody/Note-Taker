6cad95a0e19f9723f53901acf34bd416229cca2d
Sean Melody <seandmelody@gmail.com>
2020-12-10 21:00:57 -0800

    server connected

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 const express = require("express");
 const app = express();
 const PORT = 5003;
-// const apiRoutes = require("./api-routes");
-// const clientRoutes = require("./routes/client-routes");
+const apiRoutes = require("./routes/apiRoutes");
+const clientRoutes = require("./routes/clientRoutes");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use("/api", apiRoutes);
-// app.use("/", clientRoutes);
+app.use("/api", apiRoutes);
+app.use("/", clientRoutes);
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
