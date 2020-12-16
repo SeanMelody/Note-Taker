5a2cad5b0edaa91406a3c9e54547d8527850f340
Sean Melody <seandmelody@gmail.com>
2020-12-15 21:52:11 -0800

    Added descriptive commits to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,85 +1,57 @@
+// Consts express and fs
 const router = require("express").Router();
 const fs = require("fs");
 
-
-// router.get("/notes", (req, res) => {
-
-// })
-
-// router.get("/api/notes", (req, res) => {
-//     fs.readFile("../db/db.json", "utf8", (err, data) => {
-//         if (err) throw err;
-//         // res.json(JSON.parse(data));
-//         console.log(data)
-//     });
-// });
-
+// Route Get Request to get the notes
 router.get("/notes", (req, res) => {
-
+    // Read the db.json file
     fs.readFile("./db/db.json", "utf8", (err, data) => {
+        // Get those errors
         if (err) throw err;
+        // pase that data!
         res.send(JSON.parse(data))
     })
-    // getNotes()
 })
 
 
 
-
+// Router Post Request to post the new note written
 router.post("/notes", (req, res) => {
-    // console.log(req.body)
-    // res.send("notestest working")
+    // fs read the db.json file
     fs.readFile("./db/db.json", "utf8", (err, data) => {
+        // no errors allowed
         if (err) throw err;
-        // res.json(JSON.parse(data));
+        // set the new note to data to a variable
         const newNote = JSON.parse(data);
+        // push the new note to db.json file
         newNote.push({
             title: req.body.title,
             text: req.body.text,
         })
 
-        // console.log(noteTest)
+        // Write a new db.json file with the new note
         fs.writeFile("./db/db.json", JSON.stringify(newNote), (err) => {
+            // thrownin at them errors
             if (err) throw err;
+            // read the new db.json file
             fs.readFile("./db/db.json", "utf8", (err, data) => {
+                // Thowing all them errors
                 if (err) throw err;
+                // send that Json Parsed Data
                 res.send(JSON.parse(data))
             })
+            // return the write Function
             return
-
-            // res.json({ msg: "successfully added" })
         })
     })
 });
 
-// Post a past written note to the screen
-
-// router.get("/notes:routename", (req, res) => {
-//     fs.readFile("./db/db.json", "utf8", (err, data) => {
-//         if (err) throw err;
-//         const allNotes = JSON.parse(data);
-//         const search = req.params.routename;
-//         console.log(search)
-//         for (let i = 0; i < allNotes.length; i++) {
-//             if (allNotes[i] === search) {
-//                 console.log(allNotes[i])
-//                 // return res.json(allNotes[i]);
-//                 // console.log(res.json(allNotes[i]));
-//             }
-//         }
-//         // return res.json({
-//         //     msg: "the character you are searching for does not exist",
-//         //     error: `attempted route: ${req.params.routename}`,
-//         // });
-//     });
-// });
-
-
 router.delete("/notes", (req, res) => {
     console.log("delete Me")
-    fs.unlink("./db/db.json", (err, data) => {
 
-    })
+    // fs.unlink("./db/db.json", (err, data) => {
+
+    // })
     // fs.readFile("./db/db.json", "utf8", (err, data) => {
     //     if (err) throw err;
     //     const allNotes = JSON.parse(data);
@@ -103,6 +75,5 @@ router.delete("/notes", (req, res) => {
     })
 })
 
-//Unlink
 
 module.exports = router;
