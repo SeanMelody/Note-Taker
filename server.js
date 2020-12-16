c4886a3134ae28de995420a96139019e81cd0158
Sean Melody <seandmelody@gmail.com>
2020-12-15 20:21:00 -0800

    More Descriptive Comments Added

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const PORT = 5003;
 const apiRoutes = require("./routes/apiRoutes");
 const clientRoutes = require("./routes/clientRoutes");
 
-
+// Required to make sure the html can be read
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//NEED THIS TO BE ABLE TO USE THE JS AND CSS!!!!
 app.use(express.static("public"))
 
+// Middleware for where the routes go and giving them variables
 app.use("/api", apiRoutes);
 app.use("/", clientRoutes);
 
+// let the user know that the local host is working and at what port!
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
